cce5e6c3047c047ee2263f50e45b8722c87f5c0a
Milson Fortunato Neto <milsonfn@yahoo.com>
2025-04-06 04:53:54 -0300

    containerGrafico

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -3,7 +3,8 @@ import { Button } from "../../components/button";
 import { Input } from "../../components/input";
 import { Logo } from "../../components/logo";
 import { Title } from "../../components/title";
-import { Balance, Filters, Header, InputGroup, Main, Section } from "./styles";
+import { Balance, ChartAction, ChartContainer, ChartContent, 
+    Filters, Header, InputGroup, Main, Section } from "./styles";
 import { ButtonIcon } from "../../components/button-icon";
 import { Card } from "../../components/card";
 
@@ -24,15 +25,19 @@ export function Home() {
                 <InputGroup>
                     <InputMask
                         component={Input}
-                        mask="dd/mm/yyyy"
-                        replacement={{d:/\d/, m:/\d/, y:/\d/}}
-                        variant="dark" label="Início"
+                        mask="dd/mm/aaaa"
+                        replacement={{d:/\d/, m:/\d/, a:/\d/}}
+                        variant="dark" 
+                        label="Início"
+                        placeholder="dd/mm/aaaa"
                     />
                     <InputMask
                         component={Input}
-                        mask="dd/mm/yyyy"
-                        replacement={{d:/\d/, m:/\d/, y:/\d/}}
-                        variant="dark" label="Fim"
+                        mask="dd/mm/aaaa"
+                        replacement={{d:/\d/, m:/\d/, a:/\d/}}
+                        variant="dark" 
+                        label="Fim"
+                        placeholder="dd/mm/aaaa"
                     />
                     <ButtonIcon/>                    
                 </InputGroup>
@@ -42,6 +47,35 @@ export function Home() {
                 <Card title="Saldo" amount={1000000} variant="incomes"/>
                 <Card title="Saldo" amount={1000000} variant="expenses"/>
             </Balance>
+            <ChartContainer>
+                <header>
+                    <Title 
+                    title="Gastos" 
+                    subtitle="Despesas por categoria no período"
+                    />
+                </header>
+                <ChartContent></ChartContent>
+            </ChartContainer>
+            <ChartContainer>
+                <header>
+                    <Title 
+                    title="Evolução Financeira" 
+                    subtitle="Saldo, Receita e Gastos no ano"
+                    />
+                    <ChartAction>
+                        <InputMask
+                            component={Input}
+                            mask="aaaa"
+                            replacement={{a:/\d/}}
+                            variant="black" 
+                            label="Ano"
+                            placeholder="aaaa"
+                        />
+                        <ButtonIcon/> 
+                    </ChartAction>
+                </header>
+                <ChartContent></ChartContent>
+            </ChartContainer>
         </Section>
     </Main>
     </>
